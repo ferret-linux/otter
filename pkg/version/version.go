d827c458f41d696478f82a3c9050ba1f9d373acb
itzNOELdev <dev.zodium.project@gmail.com>
2026-05-10 23:29:41 +0530

    Fix : version.go

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,4 +1,4 @@
 package version
 
 // Version is set at build time via -ldflags.
-var Version = "dev" //nolint:gochecknoglobals // default value for development builds
+var Version = "unstable" //nolint:gochecknoglobals // default value for development builds
