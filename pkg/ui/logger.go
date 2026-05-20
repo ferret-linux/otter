7ce1b81aa129ce2e40e4f2429c78783d4c522205
itzNOELdev <dev.zodium.project@gmail.com>
2026-05-20 11:12:10 +0530

    Add : initial logger.go

---
+++ pkg/ui/logger.go
@@ -0,0 +1,46 @@
+package ui
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+type Logger struct {
+	writer io.Writer
+}
+
+func NewLogger(writer io.Writer) *Logger {
+	return &Logger{writer: writer}
+}
+
+// DefaultLogger writes to stderr
+var DefaultLogger = NewLogger(os.Stderr)
+
+func (l *Logger) Ok(msg string, a ...any) {
+	icon := colorGreen + "[✓]" + colorReset
+	fmt.Fprintf(l.writer, "%s %s\n", icon, fmt.Sprintf(msg, a...))
+}
+
+func (l *Logger) Error(msg string, a ...any) {
+	icon := colorRed + "[✗]" + colorReset
+	text := colorRed + fmt.Sprintf(msg, a...) + colorReset
+	fmt.Fprintf(l.writer, "%s %s\n", icon, text)
+}
+
+func (l *Logger) Warn(msg string, a ...any) {
+	icon := colorYellow + "[⚠]" + colorReset
+	fmt.Fprintf(l.writer, "%s %s\n", icon, fmt.Sprintf(msg, a...))
+}
+
+func (l *Logger) Info(msg string, a ...any) {
+	icon := colorCyan + "[ℹ]" + colorReset
+	text := colorDim + fmt.Sprintf(msg, a...) + colorReset
+	fmt.Fprintf(l.writer, "%s %s\n", icon, text)
+}
+
+func (l *Logger) Notice(msg string, a ...any) {
+	icon := colorTeal + "[‣]" + colorReset
+	text := colorDim + fmt.Sprintf(msg, a...) + colorReset
+	fmt.Fprintf(l.writer, "%s %s\n", icon, text)
+}
