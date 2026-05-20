cb60cdf4e2c19108c6d8722236675bbdcd9cb22d
itzNOELdev <dev.zodium.project@gmail.com>
2026-05-20 10:48:51 +0530

    Feat : treat .ini file as positional arg

--- internal/cli/assemble.go
+++ internal/cli/assemble.go
@@ -68,10 +68,10 @@ func assembleAction(ctx context.Context, cmd *cli.Command, cfg *config.Values, d
 		return errors.New("container manager not found in context")
 	}
 
-	// TODO: handle file name as a positional argument
-	// https://github.com/89luca89/distrobox-next/blob/07b3abf2015effafc5596b9dc7f02c35a17eb8a7/otter-assemble#L205
-
 	manifestFilePath := cmd.String("file")
+	if manifestFilePath == "" && cmd.Args().Len() > 0 {
+		manifestFilePath = cmd.Args().First()
+	}
 	if manifestFilePath == "" {
 		manifestFilePath = "./otter.ini"
 	}
