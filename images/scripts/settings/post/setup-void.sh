38295f39e86a8a2d546229f7229dec8077e32f6e
itzNOELdev <dev.zodium.project@gmail.com>
2026-06-01 11:34:46 +0530

    Add : new scripts

---
+++ images/scripts/settings/post/setup-void.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# This file is part of the otter project:
+#    https://github.com/ferret-linux/otter
+# Copyright (C) 2026 otter contributors
+
+set -e
+
+# Add otter image marker
+touch /usr/lib/otter/container.official
