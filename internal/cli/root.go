afc2cb958c6a0243ba19e03236388ae82ffdf63b
itzNOELdev <dev.zodium.project@gmail.com>
2026-08-01 23:34:07 +0530

    Add : os & arch to --version ( to tell what it was compiled on )

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -35,8 +35,10 @@ func NewRootCommand(cfg *config.Values) *cli.Command {
 	cli.VersionPrinter = func(cmd *cli.Command) {
 		root := cmd.Root()
 		fmt.Fprintf(root.Writer, "%s %s\n\nInfo\n", root.Name, root.Version)
-		fmt.Fprintf(root.Writer, "  → commit     : %s\n", commit)
+		fmt.Fprintf(root.Writer, "  → os         : %s\n", runtime.GOOS)
+		fmt.Fprintf(root.Writer, "  → platform   : %s\n", runtime.GOARCH)
 		fmt.Fprintf(root.Writer, "  → version    : %s\n", root.Version)
+		fmt.Fprintf(root.Writer, "  → git commit : %s\n", commit)
 		fmt.Fprintf(root.Writer, "  → go version : %s\n", runtime.Version())
 		fmt.Fprintf(root.Writer, "  → build time : %s\n", buildTime)
 	}
