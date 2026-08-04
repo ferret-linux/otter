0cd26c3fbed9a7a375d59a7d0bc060b9d3e081ab
itzNOELdev <dev.zodium.project@gmail.com>
2026-08-04 13:33:34 +0530

    Fix : upgrade being aborted in locked case

--- pkg/commands/upgrade.go
+++ pkg/commands/upgrade.go
@@ -80,11 +80,8 @@ func (c *UpgradeCommand) Execute(ctx context.Context, opts *UpgradeOptions) erro
 
 	for _, name := range containerNames {
 		if isLocked(ctx, c.containerManager, name) {
-			if opts.All || opts.Running {
-				ui.DefaultLogger.Warn("'%s' is locked, skipping", name)
-				continue
-			}
-			return fmt.Errorf("'%s' is locked, run 'otter unlock %s' first", name, name)
+			ui.DefaultLogger.Warn("'%s' is locked, skipping", name)
+			continue
 		}
 		if err := c.upgradeContainer(ctx, name); err != nil {
 			lastErr = fmt.Errorf("failed while upgrading %s: %w", name, err)
