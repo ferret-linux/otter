b0521c8c818cc7d0373536eaf4b9ba22b5527b49
itzNOELdev <dev.zodium.project@gmail.com>
2026-08-05 23:09:14 +0530

    Fix : common dir's in OCI/Otter images

--- images/scripts/setup-common.sh
+++ images/scripts/setup-common.sh
@@ -8,8 +8,17 @@
 # Usage: ./setup-common.sh
 # it's used to setup common settings/defaults that are generic across distros
 
+# Remove broken dir-files from images
+rm -rf /opt /root
+
 # Make common otter specific dir's
 mkdir -p \
+    /opt \
+    /run \
+    /tmp \
+    /var \
+    /root \
+    /home \
     /usr/libexec/ \
     /etc/profile.d \
     /etc/sudoers.d \
@@ -17,6 +26,15 @@ mkdir -p \
     /usr/local/bin \
     /usr/lib/otter/scripts \
     /usr/lib/otter/helpers 
+
+# Fix Permissions for dirs
+chmod 700 /root
+chmod 755 /opt
+chmod 755 /usr/local/bin
+chmod 755 /usr/libexec
+chmod 755 /etc/profile.d
+chmod 750 /etc/sudoers.d
+chmod 1777 /tmp /var/tmp
 
 # Add generic image identifier
 touch /usr/lib/otter/container.official
