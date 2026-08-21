7338dffe28bd60c7c741c98cd1f4ea1fff42c82b
itzNOELdev <dev.zodium.project@gmail.com>
2026-08-21 08:29:43 +0530

    Fix : su-pty wrapper

--- internal/insidecontainer/assets/initialization-scripts/configs/su-pty-wrapper.sh
+++ internal/insidecontainer/assets/initialization-scripts/configs/su-pty-wrapper.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 for i do
-  [ "$i" = --pty ] || set -- "$@" "$i"
+  [ "${i}" = --pty ] || set -- "$@" "${i}"
   shift
 done
 
+# shellcheck disable=SC2117 # forwarding an interactive `su` invocation as-is (minus --pty), not running a fixed command via su
 /bin/su "$@"
